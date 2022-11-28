91b1179d3104744e663f27224575949d3e77c196
Rjames13 <70821234+Rjames13@users.noreply.github.com>
2022-11-28 18:20:20 -0500

    lots of organization, can connect

---
+++ assimentcommand/README.md
@@ -0,0 +1,3 @@
+be in assimentcommand\
+compile with "mvn package"\
+run with " mvn -q exec:java "-Dexec.mainClass=com.App" "
--- assimentcommand/src/main/java/com/App.java
+++ assimentcommand/src/main/java/com/App.java
@@ -1,17 +1,15 @@
 package com;
+
 import java.sql.*;
 import java.util.Scanner;
 
 public class App {
-    String connectionUrl;
-    Connection connection;
-
 
 
     // Connect to your database.
     // Replace server name, username, and password with your credentials
     public static void main(String[] args) {
-        System.out.println("Welcome to ClassBox Command Line Inteface");
+        System.out.println("Welcome to ClassBox Command Line Interface");
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Enter username");
 
@@ -22,33 +20,231 @@ public class App {
 
         String connectionUrl =
                 "jdbc:sqlserver://robin-amd.STUDENT.CWRU.EDU:1433;"
-                        + "database=university;"
-                        + "user="+userName+";"
-                        + "password="+password+";"
+                        + "database=assignments;"
+                        + "user=" + userName + ";"
+                        + "password=" + password + ";"
                         + "encrypt=true;"
                         + "trustServerCertificate=true;"
                         + "loginTimeout=30;";
 
+
         ResultSet resultSet = null;
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(connectionUrl);
 
-        try (Connection connection = DriverManager.getConnection(connectionUrl);
-             Statement statement = connection.createStatement();) {
+            selectionmenu0();
 
-            // Create and execute a SELECT SQL statement.
-            String selectSql = "SELECT top 100 ID, Name, Dept_Name from Instructor";
-            resultSet = statement.executeQuery(selectSql);
 
-            // Print results from select statement
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString(2) + " " + resultSet.getString(3));
-            }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
+            System.exit(1);
         }
+
+
+    }
+
+    private static void selectionmenu0() {
+        // Local variable
+        int swValue;
+
+        // Display menu graphics
+        System.out.println("============================");
+        System.out.println("|   Menu 1    |");
+        System.out.println("============================");
+        System.out.println("| Options:                 |");
+        System.out.println("|        1. Option 1       |");
+        System.out.println("|        2. Option 2       |");
+        System.out.println("|        3. Exit           |");
+        System.out.println("============================");
+        swValue = Keyin.inInt(" Select option: ");
+
+        // Switch construct
+        switch (swValue) {
+            case 1:
+                System.out.println("Option 1 selected");
+                break;
+            case 2:
+                System.out.println("Option 2 selected");
+                break;
+            case 3:
+                System.out.println("Exit selected");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid selection");
+                break; // This break is not really necessary
+
+        }
+
+
+    }
+    private static void selectionmenu2() {
+        // Local variable
+        int swValue;
+
+        // Display menu graphics
+        System.out.println("============================");
+        System.out.println("|   Menu 2    |");
+        System.out.println("============================");
+        System.out.println("| Options:                 |");
+        System.out.println("|        1. Option 1       |");
+        System.out.println("|        2. Option 2       |");
+        System.out.println("|        3. Exit           |");
+        System.out.println("============================");
+        swValue = Keyin.inInt(" Select option: ");
+
+        // Switch construct
+        switch (swValue) {
+            case 1:
+                System.out.println("Option 1 selected");
+                break;
+            case 2:
+                System.out.println("Option 2 selected");
+                break;
+            case 3:
+                System.out.println("Exit selected");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid selection");
+                break; // This break is not really necessary
+
+        }
+
+
+    }
+    private static void selectionmenu3() {
+        // Local variable
+        int swValue;
+
+        // Display menu graphics
+        System.out.println("============================");
+        System.out.println("|   Menu 1    |");
+        System.out.println("============================");
+        System.out.println("| Options:                 |");
+        System.out.println("|        1. Option 1       |");
+        System.out.println("|        2. Option 2       |");
+        System.out.println("|        3. Exit           |");
+        System.out.println("============================");
+        swValue = Keyin.inInt(" Select option: ");
+
+        // Switch construct
+        switch (swValue) {
+            case 1:
+                System.out.println("Option 1 selected");
+                break;
+            case 2:
+                System.out.println("Option 2 selected");
+                break;
+            case 3:
+                System.out.println("Exit selected");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid selection");
+                break; // This break is not really necessary
+
+        }
+
+
     }
 
 
+}
 
+class Keyin {
 
+    //*******************************
+    //   support methods
+    //*******************************
+    //Method to display the user's prompt string
+    public static void printPrompt(String prompt) {
+        System.out.print(prompt + " ");
+        System.out.flush();
+    }
+
+    //Method to make sure no data is available in the
+    //input stream
+    public static void inputFlush() {
+        int dummy;
+        int bAvail;
+
+        try {
+            while ((System.in.available()) != 0)
+                dummy = System.in.read();
+        } catch (java.io.IOException e) {
+            System.out.println("Input error");
+        }
+    }
+
+    //********************************
+    //  data input methods for
+    //string, int, char, and double
+    //********************************
+    public static String inString(String prompt) {
+        inputFlush();
+        printPrompt(prompt);
+        return inString();
+    }
+
+    public static String inString() {
+        int aChar;
+        String s = "";
+        boolean finished = false;
+
+        while (!finished) {
+            try {
+                aChar = System.in.read();
+                if (aChar < 0 || (char) aChar == '\n')
+                    finished = true;
+                else if ((char) aChar != '\r')
+                    s = s + (char) aChar; // Enter into string
+            } catch (java.io.IOException e) {
+                System.out.println("Input error");
+                finished = true;
+            }
+        }
+        return s;
+    }
+
+    public static int inInt(String prompt) {
+        while (true) {
+            inputFlush();
+            printPrompt(prompt);
+            try {
+                return Integer.valueOf(inString().trim()).intValue();
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Not an integer");
+            }
+        }
+    }
+
+    public static char inChar(String prompt) {
+        int aChar = 0;
+
+        inputFlush();
+        printPrompt(prompt);
+
+        try {
+            aChar = System.in.read();
+        } catch (java.io.IOException e) {
+            System.out.println("Input error");
+        }
+        inputFlush();
+        return (char) aChar;
+    }
+
+    public static double inDouble(String prompt) {
+        while (true) {
+            inputFlush();
+            printPrompt(prompt);
+            try {
+                return Double.valueOf(inString().trim()).doubleValue();
+            } catch (NumberFormatException e) {
+                System.out
+                        .println("Invalid input. Not a floating point number");
+            }
+        }
+    }
 }
